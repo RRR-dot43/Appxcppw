c1aa22dad9cb7c24add8f1b58faf12540889739e
RRR-dot43 <ruhilrohit10@gmail.com>
2025-04-27 23:06:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "21216779"
+api_hash = "fdad88238287459c6dc790658f1008c1"
+bot_token = "8158717275:AAFSOiFSfIeIsd0Fb_8kzVCuCV58Kw2as9Y"
+auth_users = [728093729]
